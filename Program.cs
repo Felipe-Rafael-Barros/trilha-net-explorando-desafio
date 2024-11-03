70a89d227f54bd679033d33c34d0fd08f14ed3d3
Felipe-Rafael-Barros <gitfelipebarros@gmail.com>
2024-11-02 21:45:43 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,25 +1,131 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using DesafioProjetoHospedagem.Models;
+using System.Collections.Generic;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-// Cria os modelos de hóspedes e cadastra na lista de hóspedes
-List<Pessoa> hospedes = new List<Pessoa>();
+// Função para cadastrar hóspedes
+List<Pessoa> CadastrarHospedes()
+{
+    List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+    Console.WriteLine("Digite o número de hóspedes:");
+    int numeroHospedes = int.Parse(Console.ReadLine());
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+    for (int i = 1; i <= numeroHospedes; i++)
+    {
+        Console.WriteLine($"Digite o nome do hóspede {i}:");
+        string nome = Console.ReadLine();
+        hospedes.Add(new Pessoa(nome: nome));
+    }
 
-// Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+    return hospedes;
+}
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+// Função para cadastrar uma suíte
+Suite CadastrarSuite()
+{
+    Console.WriteLine("Digite o tipo de suíte:");
+    string tipoSuite = Console.ReadLine();
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+    Console.WriteLine("Digite a capacidade da suíte:");
+    int capacidade = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o valor da diária da suíte:");
+    decimal valorDiaria = decimal.Parse(Console.ReadLine());
+
+    return new Suite(tipoSuite: tipoSuite, capacidade: capacidade, valorDiaria: valorDiaria);
+}
+
+// Função para cadastrar uma reserva
+Reserva CadastrarReserva(List<Pessoa> hospedes, Suite suite)
+{
+    Console.WriteLine("Digite o número de dias reservados:");
+    int diasReservados = int.Parse(Console.ReadLine());
+
+    Reserva reserva = new Reserva(diasReservados: diasReservados);
+    reserva.CadastrarSuite(suite);
+    reserva.CadastrarHospedes(hospedes);
+
+    return reserva;
+}
+
+// Função para avaliar uma reserva
+void AvaliarReserva(Reserva reserva)
+{
+    Console.WriteLine("Digite sua avaliação para a reserva (de 1 a 5):");
+    int avaliacao = int.Parse(Console.ReadLine());
+    
+    // Aqui você pode implementar lógica para salvar a avaliação
+    // Exemplo: reserva.Avaliacao = avaliacao;
+
+    Console.WriteLine($"Avaliação registrada: {avaliacao} estrelas");
+}
+
+// Lista para armazenar todas as reservas
+List<Reserva> reservas = new List<Reserva>();
+
+// Execução do programa
+bool continuar = true;
+
+while (continuar)
+{
+    Console.WriteLine("Escolha uma opção:");
+    Console.WriteLine("1 - Cadastrar nova reserva");
+    Console.WriteLine("2 - Listar reservas");
+    Console.WriteLine("3 - Avaliar uma reserva");
+    Console.WriteLine("4 - Sair");
+    
+    int opcao = int.Parse(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1:
+            List<Pessoa> hospedes = CadastrarHospedes();
+            Suite suite = CadastrarSuite();
+            Reserva novaReserva = CadastrarReserva(hospedes, suite);
+            reservas.Add(novaReserva);
+            Console.WriteLine("Reserva cadastrada com sucesso!");
+            break;
+
+        case 2:
+            Console.WriteLine("Reservas cadastradas:");
+            for (int i = 0; i < reservas.Count; i++)
+            {
+                Console.WriteLine($"Reserva {i + 1}:");
+                Console.WriteLine($" - Hóspedes: {reservas[i].ObterQuantidadeHospedes()}");
+                Console.WriteLine($" - Valor diária: {reservas[i].CalcularValorDiaria()}");
+                Console.WriteLine();
+            }
+            break;
+
+        case 3:
+            Console.WriteLine("Escolha o número da reserva que deseja avaliar:");
+            for (int i = 0; i < reservas.Count; i++)
+            {
+                Console.WriteLine($"Reserva {i + 1}");
+            }
+
+            int indiceReserva = int.Parse(Console.ReadLine()) - 1;
+
+            if (indiceReserva >= 0 && indiceReserva < reservas.Count)
+            {
+                AvaliarReserva(reservas[indiceReserva]);
+            }
+            else
+            {
+                Console.WriteLine("Número de reserva inválido.");
+            }
+            break;
+
+        case 4:
+            continuar = false;
+            Console.WriteLine("Encerrando o sistema.");
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida, tente novamente.");
+            break;
+    }
+}
